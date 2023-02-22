1aa3141f462816e600bb5c9cc598864c40a01fc0
Rudra Narayan Panda <rudranarayan20449@gmail.com>
2023-02-22 19:28:09 +0530

    Step -1 Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_pattern/pages/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
+      home: HomePage(),
     );
   }
 }
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -3,14 +3,72 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'dart:math' as math show Random;
 import 'package:bloc/bloc.dart';
 
-class HomePage extends StatelessWidget {
+const names = ['Rudra', 'Narayan', 'Panda'];
+
+extension RandomElement<T> on Iterable<T> {
+  T getRandomElement() => elementAt(math.Random().nextInt(length));
+}
+
+class NamesCubit extends Cubit<String?> {
+  NamesCubit() : super(null);
+
+  void pickRandomName() => emit(names.getRandomElement());
+}
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final NamesCubit cubit;
+  @override
+  void initState() {
+    super.initState();
+    cubit = NamesCubit();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    cubit.close();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: StreamBuilder<String?>(
+          stream: cubit.stream,
+          builder: (context, snapshot) {
+            final button = TextButton(
+              onPressed: () => cubit.pickRandomName(),
+              child: const Text('Pick a Random Name'),
+            );
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                return button;
+              case ConnectionState.waiting:
+                return button;
+              case ConnectionState.active:
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(snapshot.data ?? ''),
+                    button,
+                  ],
+                );
+              case ConnectionState.done:
+                return const SizedBox();
+            }
+          },
+        ),
       ),
     );
   }
